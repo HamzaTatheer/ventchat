5b72e238cd585c0f27d27a34a7293d4b1fdfb245
hamzatatheerr <79835317+hamzatatheerr@users.noreply.github.com>
2021-03-28 18:40:10 +0500

    page linked and edits

--- webapp/src/views/App.jsx
+++ webapp/src/views/App.jsx
@@ -5,15 +5,22 @@ import React from "react";
 
 //Components
 import VentChat from "./VentChat"
+import Therapy from "./Therapy"
 
 function App() {
 
 	return (
-		<div className="App">
-			<Header/>
+		<div style={{position:"relative",width:"100%",minHeight:"100vh"}} className="App">
 			<Router>
+				<Header/>
+				<div className="footer-container">
+					<span className="link" style={{paddingLeft:"10px",paddingRight:"10px"}}>Privacy</span> |
+					<span className="link" style={{paddingLeft:"10px",paddingRight:"10px"}}>About Us</span> |
+					<span className="link" style={{paddingLeft:"10px",paddingRight:"10px"}}>Contact Us</span>
+				</div>
 				<Switch>
 					<Route exact path="/" component={VentChat} />
+					<Route exact path="/therapy" component={Therapy} />
 				</Switch>
 			</Router>
 		</div>
